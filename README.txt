bab5a54451a3890d90145ea1fd762e11ba5052c4
audrasjb <[audrasjb@gmail.com]>
2016-12-13 20:22:16 +0100

    initial commit

---
+++ README.txt
@@ -0,0 +1,36 @@
+=== Bring back text justify ===
+Contributors: audrasjb
+Tags: justify, justification, justifier, tinymce, 4.7, text
+Requires at least: 4.7
+Tested up to: 4.7
+Stable tag: 1.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Oh noes, WP4.7 stole my justify text button ! Please bring it back (but keep underlining away, it sucks)
+
+== Description ==
+
+This plugin should be used to supply abbreviation support in the TinyMCE visual editor. Once activated, it provides an "ABR" button which allows editor to indicate the meaning of each abbreviation used in Pages, Posts or any custom post types used in your WordPress installation. The markup is fully compatible with accessibility rules like Web Content Accessibility Guidelines (WCAG). It uses `<abbr>…</abbr>` HTML markup instead of the deprecated `<acronym>…</acronym>` HTML markup.
+
+**Credits**
+
+[Jean-Baptiste Audras](http://jeanbaptisteaudras.com/ "Jean-Baptiste Audras"), project manager at [Whodunit](http://www.whodunit.fr/ "Whodunit Web Agency") and WordPress developer. @audrasjb on [Twitter](https://twitter.com/audrasjb "@audrasjb on Twitter") or [Github](https://github.com/audrasjb "@audrasjb on Github").
+
+== Installation ==
+
+English:
+1. Install the plugin and activate it. Text justify alignment button is back (in the second row of buttons)
+
+== Frequently Asked Questions ==
+
+= Why I cannot see any changes on my website front-end? =
+
+== Screenshots ==
+
+1. Hurray!
+
+== Changelog ==
+
+= 1.0 =
+* Plugin initial commit.
---
+++ justify-back.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @link              http://jeanbaptisteaudras.com/justify-back/
+ * @since             1.0
+ * @package           Bring back text justify
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Bring back text justify
+ * Plugin URI:        http://jeanbaptisteaudras.com/justify-back/
+ * Description:       Oh noes, WP4.7 stole my justify text button ! Please bring it back (but keep underlining away, it sucks)
+ * Version:           1.0
+ * Author:            Jean-Baptiste Audras, project manager @ Whodunit
+ * Author URI:        http://jeanbaptisteaudras.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       justify-back
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class justifyBack {
+	function __construct() {
+		if ( is_admin() ) {
+			add_action( 'init', array(  $this, 'setup_justifyBack' ) );
+		}
+	}
+	function setup_justifyBack() {
+		// Check if the logged in WordPress User can edit Posts or Pages
+		// If not, don't register
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+        	return;
+		}
+		// Check if the logged in WordPress User has the Visual Editor enabled
+		// If not, don't register
+		if ( get_user_option( 'rich_editing' ) !== 'true' ) {
+			return;
+		}
+		function mce_justifyBack( $mce_buttons ) {	
+			$mce_buttons[] = 'alignjustify';
+			return $mce_buttons;
+		}	
+		add_filter( 'mce_buttons_2', 'mce_justifyBack', 5 );
+	}
+}
+$justifyBack = new justifyBack;
